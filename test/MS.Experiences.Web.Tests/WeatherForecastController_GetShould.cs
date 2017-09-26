087a39684f98823a1c374aad7f5892100887046f
Simon Dib <dib.simon@gmail.com>
2017-09-26 22:49:29 +0200

    test: Add test on api controller

---
+++ test/MS.Experiences.Web.Tests/WeatherForecastController_GetShould.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MS.Experiences.Weather;
+using MS.Experiences.Web.Controllers;
+
+namespace MS.Experiences.Web.Tests
+{
+    [TestClass]
+    public class WeatherForecastController_GetShould
+    {
+        private WeatherForecastController controller;
+        private Mock<IWeatherForecastService> weatherService;
+        private Mock<IWeatherDataServiceFactory> factory;
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            weatherService = new Mock<IWeatherForecastService>();
+            factory = new Mock<IWeatherDataServiceFactory>();
+            controller = new WeatherForecastController(weatherService.Object, factory.Object);
+        }
+
+        [TestMethod]
+        public void ReturnServiceData()
+        {
+            var serviceData = new WeatherForecast[0];
+            weatherService.Setup(o => o.GetWeatherForecasts(1))
+                .Returns(serviceData);
+
+            IEnumerable<WeatherForecast> forecasts = controller.Get(1);
+
+            Assert.AreSame(serviceData, forecasts);
+        }
+    }
+}
