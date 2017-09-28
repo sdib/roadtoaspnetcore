b5928548a84c7931181e411a6582dd712e39dd64
Simon Dib <dib.simon@gmail.com>
2017-09-28 19:08:16 +0200

    feat: Add loggerfactory holder

---
+++ src/MS.Experiences.Migration/Logs/LoggerFactoryHolder.cs
@@ -0,0 +1,18 @@
+﻿using AF.AxaConnect.Infrastructure.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace MS.Experiences.Migration.Logs
+{
+    public static class LoggerFactoryHolder
+    {
+        private static ILoggerFactory _loggerFactory;
+
+        static LoggerFactoryHolder()
+        {
+            _loggerFactory = new LoggerFactory();
+            _loggerFactory.AddLog4Net();
+        }
+
+        public static ILoggerFactory Instance => _loggerFactory;
+    }
+}
