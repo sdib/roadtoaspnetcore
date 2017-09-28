e0dc5d8c20787482dcbab3aec413e8822f628890
Simon Dib <dib.simon@gmail.com>
2017-09-28 15:23:58 +0200

    feat: Add packages for migration

---
+++ src/MS.Experiences.Migration/Logs/HostingEnvironmentExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace AF.AxaConnect.FrontOffice.Web.Infrastructure.Extensions
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using log4net;
+    using log4net.Config;
+    using Microsoft.AspNetCore.Hosting;
+
+    public static class HostingEnvironmentExtensions
+    {
+        public static void ConfigureLog4Net(this IHostingEnvironment hostingEnvironment, string configFileRelativePath, IDictionary<string, string> globalProperties)
+        {
+            foreach (KeyValuePair<string, string> keyValuePair in globalProperties)
+            {
+                GlobalContext.Properties[keyValuePair.Key] = keyValuePair.Value;
+            }
+
+            string configFilePath = Path.Combine(hostingEnvironment.ContentRootPath, configFileRelativePath);
+            if (!File.Exists(configFilePath))
+            {
+                throw new FileNotFoundException($"{configFilePath} is not found");
+            }
+            FileInfo configFileInfo = new FileInfo(configFilePath);
+            XmlConfigurator.Configure(configFileInfo);
+        }
+    }
+}
---
+++ src/MS.Experiences.Migration/Logs/Log4NetLoggerProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace AF.AxaConnect.Infrastructure.Logging
+{
+    using Microsoft.Extensions.Logging;
+
+    internal class Log4NetLoggerProvider : ILoggerProvider
+    {
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new Log4NetLogger(categoryName);
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
