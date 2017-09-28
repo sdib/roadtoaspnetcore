5759895da5639633ab1516413d0f83f9625c11ad
guillaume chervet <guillaume.chervet@gmail.com>
2017-09-28 19:20:27 +0200

    feat(js) maj js for aller plus vite

--- src/MS.Experiences.Web/App/app.run.js
+++ src/MS.Experiences.Web/App/app.run.js
@@ -1,4 +1,10 @@
-﻿
+﻿//import 'angular';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+//import weather from './weather';
+//import './app.css';
+
+
 var modules = ['ms.experience.weather'];
 var app = angular.module('ms.experience', modules);
 
--- src/MS.Experiences.Web/wwwroot/dist/app.bundle.js
+++ src/MS.Experiences.Web/wwwroot/dist/app.bundle.js
@@ -70,7 +70,14 @@
 "use strict";
 
 
-var modules = ['ms.experience.wheather'];
+//import 'angular';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+//import weather from './weather';
+//import './app.css';
+
+
+var modules = ['ms.experience.weather'];
 var app = angular.module('ms.experience', modules);
 
 app.run([function () {}]);
